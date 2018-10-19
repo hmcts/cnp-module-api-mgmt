1b6d36cba56fd3c1365cf16d7370d98e537d2274
Gianluca Ciocci <gianluca.ciocci@hmcts.net>
2018-10-19 15:40:47 +0100

    subnet creation in core-environment with id passed in

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ locals {
   platform_api_mgmt_sku = "${var.env == "prod" ? "Premium" : "Developer"}"
 }
 
-resource "azurerm_subnet" "api-mgmt-subnet" {
-  name                 = "core-infra-subnet-apimgmt-${var.env}"
-  resource_group_name  = "${var.vnet_rg_name}"
-  virtual_network_name = "${var.vnet_name}"
-  address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
+# resource "azurerm_subnet" "api-mgmt-subnet" {
+#   name                 = "core-infra-subnet-apimgmt-${var.env}"
+#   resource_group_name  = "${var.vnet_rg_name}"
+#   virtual_network_name = "${var.vnet_name}"
+#   address_prefix       = "${cidrsubnet("${var.source_range}", 4, var.source_range_index)}"
 
-  lifecycle {
-    ignore_changes = "address_prefix"
-  }
-}
+#   lifecycle {
+#     ignore_changes = "address_prefix"
+#   }
+# }
 
 resource "azurerm_template_deployment" "api-managment" {
   template_body       = "${data.template_file.apimgmttemplate.rendered}"
@@ -31,7 +31,7 @@ resource "azurerm_template_deployment" "api-managment" {
     notification_sender_email          = "${var.notification_sender_email}"
     env                                = "${var.env}"
     platform_api_mgmt_name             = "${local.name}"
-    platform_api_mgmt_subnetResourceId = "${azurerm_subnet.api-mgmt-subnet.id}"
+    platform_api_mgmt_subnetResourceId = "${var.api_subnet_id}"
     platform_api_mgmt_sku              = "${local.platform_api_mgmt_sku}"
     teamName                           = "${lookup(var.common_tags, "Team Name")}"
   }
