56d2ee0f70a3dfb8c298ed52ec108b526a28e640
naikajah <piyush.m82@gmail.com>
2020-09-08 18:11:33 +0100

    update api mgmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_subnet" "api-mgmt-subnet" {
   name                 = "core-infra-subnet-apimgmt-${var.env}"
   resource_group_name  = var.vnet_rg_name
   virtual_network_name = var.vnet_name
-  address_prefix       = cidrsubnet(var.source_range, 4, var.source_range_index)
+  address_prefixes     = [ "${cidrsubnet(var.source_range, 4, 4)}" ]
 
   lifecycle {
     ignore_changes = [address_prefix]
@@ -15,16 +15,17 @@ resource "azurerm_subnet" "api-mgmt-subnet" {
 }
 
 resource "azurerm_api_management" "api-managment" {
-  name                = "${local.name}"
-  location            = "${var.location}"
-  resource_group_name = "${var.vnet_rg_name}"
-  publisher_name      = "${var.publisher_name}"
-  publisher_email     = "${var.publisher_email}"
-  notification_sender_email = "${var.notification_sender_email}"
+  name                      = local.name
+  location                  = var.location
+  resource_group_name       = var.vnet_rg_name
+  publisher_name            = var.publisher_name
+  publisher_email           = var.publisher_email
+  notification_sender_email = var.notification_sender_email
+  virtual_network_type      = "Internal"
 
-  virtual_network_configuration {
+  virtual_network_configuration  {
     subnet_id = azurerm_subnet.api-mgmt-subnet.id
   }
 
-  sku_name = "${local.platform_api_mgmt_sku}"
+  sku_name = local.platform_api_mgmt_sku
 }
--- variables.tf
+++ variables.tf
@@ -1,39 +1,30 @@
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
-variable "env" {
-  type = "string"
-}
+variable "env" {}
 
-variable "vnet_rg_name" {
-  type = "string"
-}
+variable "vnet_rg_name" {}
 
-// variable "api_subnet_id" {
-//   type = "string"
-// }
+variable "vnet_name" {}
 
 variable "source_range_index" {}
 
+variable "source_range" {}
+
 variable "publisher_email" {
-  type    = "string"
   default = "api-mangement@hmcts.net"
 }
 
 variable "publisher_name" {
-  type    = "string"
   default = "HMCTS Reform Platform Engineering"
 }
 
 variable "notification_sender_email" {
-  type    = "string"
   default = "apimgmt-noreply@mail.windowsazure.com"
 }
 
 variable "common_tags" {
-  type = "map"
   default = {
     "managedBy" = "pleaseTagMe"
   }
